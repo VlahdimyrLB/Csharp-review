54b55c6de842ca0cec34f1045e2cc3967e299169
VlahdimyrLB <bambalanvlahdimyr@gmail.com>
2024-06-17 06:26:25 +0800

    Added CRUDpractice1
    This is a starter practice everythings seems basic here

---
+++ CRUDpractice1/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace CRUDpractice1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.ageTextBox = new System.Windows.Forms.TextBox();
+            this.insertButton = new System.Windows.Forms.Button();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.deleteTextBox = new System.Windows.Forms.Button();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(18, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(18, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Age:";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(97, 27);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(148, 20);
+            this.nameTextBox.TabIndex = 4;
+            // 
+            // ageTextBox
+            // 
+            this.ageTextBox.Location = new System.Drawing.Point(97, 57);
+            this.ageTextBox.Name = "ageTextBox";
+            this.ageTextBox.Size = new System.Drawing.Size(148, 20);
+            this.ageTextBox.TabIndex = 5;
+            // 
+            // insertButton
+            // 
+            this.insertButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.insertButton.Location = new System.Drawing.Point(410, 124);
+            this.insertButton.Name = "insertButton";
+            this.insertButton.Size = new System.Drawing.Size(96, 31);
+            this.insertButton.TabIndex = 6;
+            this.insertButton.Text = "INSERT";
+            this.insertButton.UseVisualStyleBackColor = true;
+            this.insertButton.Click += new System.EventHandler(this.insertButton_Click);
+            // 
+            // updateButton
+            // 
+            this.updateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.updateButton.Location = new System.Drawing.Point(410, 161);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(96, 31);
+            this.updateButton.TabIndex = 7;
+            this.updateButton.Text = "UPDATE";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.Location = new System.Drawing.Point(97, 88);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.Size = new System.Drawing.Size(148, 20);
+            this.idTextBox.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(18, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "ID:";
+            // 
+            // deleteTextBox
+            // 
+            this.deleteTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deleteTextBox.Location = new System.Drawing.Point(410, 198);
+            this.deleteTextBox.Name = "deleteTextBox";
+            this.deleteTextBox.Size = new System.Drawing.Size(96, 31);
+            this.deleteTextBox.TabIndex = 10;
+            this.deleteTextBox.Text = "DELETE";
+            this.deleteTextBox.UseVisualStyleBackColor = true;
+            this.deleteTextBox.Click += new System.EventHandler(this.deleteTextBox_Click);
+            // 
+            // searchButton
+            // 
+            this.searchButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchButton.Location = new System.Drawing.Point(410, 235);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(96, 31);
+            this.searchButton.TabIndex = 11;
+            this.searchButton.Text = "SEARCH";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(17, 124);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(387, 142);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 279);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.deleteTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.idTextBox);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.insertButton);
+            this.Controls.Add(this.ageTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox ageTextBox;
+        private System.Windows.Forms.Button insertButton;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button deleteTextBox;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ CRUDpractice1/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUDpractice1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [Obsolete]
+        private void insertButton_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-18BQ05G;Initial Catalog=CRUDpractice1;Integrated Security=True;Encrypt=True;TrustServerCertificate=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Insert into contact values(@name,@age)", con);
+            cmd.Parameters.Add("@name", nameTextBox.Text);
+            cmd.Parameters.Add("@age", int.Parse(ageTextBox.Text));
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+
+            MessageBox.Show("Data Inserted Successfully");
+            nameTextBox.Clear();
+            ageTextBox.Clear();
+
+        }
+
+        [Obsolete]
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+                SqlConnection con = new SqlConnection("Data Source=DESKTOP-18BQ05G;Initial Catalog=CRUDpractice1;Integrated Security=True;Encrypt=True;TrustServerCertificate=True");
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Update contact set name=@name, age=@age where id=@id", con);
+                cmd.Parameters.Add("@id", int.Parse(idTextBox.Text));
+                cmd.Parameters.Add("@name", nameTextBox.Text);
+                cmd.Parameters.Add("@age", int.Parse(ageTextBox.Text));
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+
+                MessageBox.Show("Data Updated Successfully");
+                idTextBox.Clear();
+                nameTextBox.Clear();
+                ageTextBox.Clear();
+            dataGridView1.RefreshEdit();
+
+        }
+
+        [Obsolete]
+        private void deleteTextBox_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-18BQ05G;Initial Catalog=CRUDpractice1;Integrated Security=True;Encrypt=True;TrustServerCertificate=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Delete contact where id=@id", con);
+            cmd.Parameters.Add("@id", int.Parse(idTextBox.Text));
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            idTextBox.Clear();
+            nameTextBox.Clear();
+            ageTextBox.Clear();
+            MessageBox.Show("Data Deleted Successfully");
+
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-18BQ05G;Initial Catalog=CRUDpractice1;Integrated Security=True;Encrypt=True;TrustServerCertificate=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from contact", con);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+
+            dataGridView1.DataSource = dataTable;
+                
+        }
+    }
+}
